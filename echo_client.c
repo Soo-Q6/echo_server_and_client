9ae60a637c4d98fb92043b853a755eaa117b5e8e
Zeno H <Zeno H>
2017-04-17 14:45:14 +0800

    initial clinet and server

--- echo_client.c
+++ echo_client.c
@@ -10,42 +10,42 @@
 #include<stdio.h>
 #define SERV_PORT 9877
 #define MAXLINE 1024
-void str_cli(FILE*fp,int sockfd);
-int main(int argc,char **argv){
-int sockfd[5];
-struct sockaddr_in servaddr;
-/*if(argc!=2){
-printf("usefjds");
-exit(0);
-}*/
+void str_cli(FILE*fp, int sockfd);
+int main(int argc, char **argv) {
+	int sockfd[5];
+	struct sockaddr_in servaddr;
+	/*if(argc!=2){
+	printf("usefjds");
+	exit(0);
+	}*/
 
-char * ipaddr="127.0.0.1";
-int i=0;
-for( i=0;i<5;i++){
-sockfd[i] =socket(AF_INET,SOCK_STREAM,0);
+	char * ipaddr = "127.0.0.1";
+	int i = 0;
+	for (i = 0; i < 5; i++) {
+		sockfd[i] = socket(AF_INET, SOCK_STREAM, 0);
 
-bzero(&servaddr,sizeof(servaddr));
-servaddr.sin_family=AF_INET;
-servaddr.sin_port=htons(SERV_PORT);
-inet_pton(AF_INET,ipaddr,&servaddr.sin_addr);
+		bzero(&servaddr, sizeof(servaddr));
+		servaddr.sin_family = AF_INET;
+		servaddr.sin_port = htons(SERV_PORT);
+		inet_pton(AF_INET, ipaddr, &servaddr.sin_addr);
 
-connect(sockfd[i],(struct sockaddr*)&servaddr,sizeof(servaddr));
+		connect(sockfd[i], (struct sockaddr*)&servaddr, sizeof(servaddr));
+	}
+	str_cli(stdin, sockfd[0]);
+	exit(0);
 }
-str_cli(stdin,sockfd[0]);
-exit(0);
-}
 
 
 
-void str_cli(FILE*fp,int sockfd){
-char sendline[MAXLINE],recvline[MAXLINE];
-while(fgets(sendline,MAXLINE,fp)!=NULL){
-write(sockfd,sendline,strlen(sendline));
-if(read(sockfd,recvline,strlen(sendline))==0){
-printf("sre_cli:error");
-exit(0);
+void str_cli(FILE*fp, int sockfd) {
+	char sendline[MAXLINE], recvline[MAXLINE];
+	while (fgets(sendline, MAXLINE, fp) != NULL) {
+		write(sockfd, sendline, strlen(sendline));
+		if (read(sockfd, recvline, strlen(sendline)) == 0) {
+			printf("sre_cli:error");
+			exit(0);
 
-}
-fputs(recvline,stdout);
-}
+		}
+		fputs(recvline, stdout);
+	}
 }
