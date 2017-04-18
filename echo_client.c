11f86778dfda745e02c02ee5412d5aac6d7572eb
Zeno H <Zeno H>
2017-04-18 15:43:56 +0800

    keepalive

--- echo_client.c
+++ echo_client.c
@@ -16,30 +16,29 @@ int main(int argc, char **argv) {
 	int sockfd;
 	struct sockaddr_in servaddr;
 	int keepalive=1;
-	/*if(argc!=2){
-	printf("usefjds");
-	exit(0);
-	}*/
+	if(argc!=2){
+		printf("usefjds");
+		exit(0);
+	}
 
-	char * ipaddr = "127.0.0.1";
-	//int i = 0;
-	//for (i = 0; i < 5; i++) {
-		//sockfd[i] = socket(AF_INET, SOCK_STREAM, 0);
-		sockfd = socket(AF_INET, SOCK_STREAM, 0);
-		bzero(&servaddr, sizeof(servaddr));
-		servaddr.sin_family = AF_INET;
-		servaddr.sin_port = htons(SERV_PORT);
-		inet_pton(AF_INET, ipaddr, &servaddr.sin_addr);
-		setsockopt(sockfd,SOL_SOCKET,SO_KEEPALIVE,&keepalive,sizeof(keepalive));
+	char *ipaddr = "127.0.0.1";
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(SERV_PORT);
+	inet_pton(AF_INET, ipaddr, &servaddr.sin_addr);
+	setsockopt(sockfd, SOL_SOCKET, SO_KEEPALIVE, &keepalive, sizeof(keepalive));
 
-		if(connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr))<0){
-			printf("connect error!\n");
-			exit(0);
-		}else{
-			printf("connect successfully!\n");
-		}
+	if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+	{
+	    printf("connect error!\n");
+	    exit(0);
+	}
+	else
+	{
+	    printf("connect successfully!\n");
+	}
 
-	//}
 	str_cli(stdin, sockfd);
 	exit(0);
 }
