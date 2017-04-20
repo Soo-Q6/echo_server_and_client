37318b30453c9abb8c01774b5fbe705b646f98b0
Zeno H <Zeno H>
2017-04-20 23:29:29 +0800

    get the ip of client

--- echo_client.c
+++ echo_client.c
@@ -2,58 +2,56 @@
 #include<sys/socket.h>
 #include<time.h>
 #include<netinet/in.h>
+#include<arpa/inet.h>
 #include<stdio.h>
 #include<string.h>
 #include<errno.h>
 #include<stdlib.h>
-#include<stdlib.h>
-#include<stdio.h>
-//#include<stdbool.h>
-#define SERV_PORT 9877
+#define SERV_PORT 8888
 #define MAXLINE 1024
-void str_cli(FILE*fp, int sockfd);
+void str_cli(FILE*fp, int sockfd,const struct sockaddr *cliaddr,socklen_t servlen);
 int main(int argc, char **argv) {
 	int sockfd;
-	struct sockaddr_in servaddr;
-	int keepalive=1;
+	struct sockaddr_in servaddr,clientaddr;
+	socklen_t clilen;
 	if(argc!=2){
 		printf("usefjds");
 		exit(0);
 	}
 
-	char *ipaddr = "127.0.0.1";
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(SERV_PORT);
-	inet_pton(AF_INET, ipaddr, &servaddr.sin_addr);
-	setsockopt(sockfd, SOL_SOCKET, SO_KEEPALIVE, &keepalive, sizeof(keepalive));
-
-	if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
-	{
-	    printf("connect error!\n");
-	    exit(0);
-	}
-	else
-	{
-	    printf("connect successfully!\n");
+	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+	if(getsockname(sockfd, (struct sockaddr *)&clientaddr, &clilen)==0){
+		if(inet_ntoa(clientaddr.sin_addr)==NULL){
+			printf("NULL\n");
+		}else{
+			printf("this client ip is %s  its port is %d\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+		}
+	}else{
+		printf("getsockname error\n");
 	}
 
-	str_cli(stdin, sockfd);
+	str_cli(stdin, sockfd,( struct sockaddr *)&servaddr,sizeof(servaddr));
 	exit(0);
 }
 
 
 
-void str_cli(FILE*fp, int sockfd) {
-	char sendline[MAXLINE], recvline[MAXLINE];
+void str_cli(FILE*fp, int sockfd,const struct sockaddr *cliaddr,socklen_t servlen) {
+	char sendline[MAXLINE], recvline[MAXLINE+1];
+	int n;
+	struct sockaddr_in caddr;
+	socklen_t clilen=sizeof(caddr);
+	//getsockname(sockfd, (struct sockaddr *)&clientaddr, &clilen);
+	//printf("this client ip is %s  its port is %d\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
 	while (fgets(sendline, MAXLINE, fp) != NULL) {
-		write(sockfd, sendline, strlen(sendline));
-		if (read(sockfd, recvline, strlen(sendline)) == 0) {
-			printf("sre_cli:error");
-			exit(0);
-
-		}
+		sendto(sockfd,sendline,strlen(sendline),0,cliaddr,servlen);
+		n=recvfrom(sockfd,recvline,MAXLINE,0,(struct sockaddr*)&caddr,&clilen);
+		printf("this client ip is %s  its port is %d\n", inet_ntoa(caddr.sin_addr), ntohs(caddr.sin_port));
+		recvline[n]=0;
 		fputs(recvline, stdout);
 	}
 }
--- echo_server.c
+++ echo_server.c
@@ -6,105 +6,52 @@
 #include<netinet/in.h>
 #include<stdlib.h>
 #include<errno.h>
-#define serv_port 9877
+#define serv_port 8888
 #define LISTENQ 1024
 #define MAXLINE 1024
-void str_echo(int sockfd);
-void sig_chid(int signo);
+void str_echo(int sockfd,struct sockaddr_in *cliaddr,socklen_t clilen);
+// void sig_chid(int signo);
 int main()
 {
 	int listenid, connfd;
-	pid_t childpid;
-	socklen_t clilen;
-	int keepalive=1;
-	//int reuse=1;
-	struct linger LINGER;
 	struct sockaddr_in cliaddr, servaddr;
-	LINGER.l_onoff=1;
-	LINGER.l_linger=0;
 
-	if ((listenid = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+	if ((listenid = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
 		printf("socket errro");
 		exit(0);
 	}
-	setsockopt(listenid,SOL_SOCKET,SO_KEEPALIVE,&keepalive,sizeof(keepalive));
-	// if(setsockopt(listenid,SOL_SOCKET,SO_REUSEPORT,&reuse,sizeof(reuse))==-1){
-	// 	printf("%s\n",strerror(errno));
-	// 	exit(0);
-	// }else{
-	// 	printf("set socket successfully\n");
-	// }
-
-	// if(setsockopt(listenid,SOL_SOCKET,SO_LINGER,&LINGER,sizeof(LINGER))==-1){
-	// 	printf("%s\n",strerror(errno));
-	// 	exit(0);
-	// }else{
-	// 	printf("set socket successfully\n");
-	// }
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port = htons(serv_port);
-	if (bind(listenid, (const struct sockaddr*)&servaddr, sizeof(servaddr)) == -1) {
+	if (bind(listenid, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1) {
 		printf("bind:%s(errno:%d)\n", strerror(errno), errno);
 		exit(0);
 	}
 
-	if (listen(listenid, LISTENQ) == -1) {
-		printf("listen:%s(errno:%d)\n", strerror(errno), errno);
-		exit(0);
-	}
-	else {
-		printf("waiting for new connection\n");
-	}
-	if (signal(SIGCHLD, sig_chid) < 0) {
-		printf("%s,%d", strerror(errno), errno);
-	}
 
-	while (1) {
-		clilen = sizeof(cliaddr);
-		connfd = accept(listenid, (struct sockaddr*)&cliaddr, &clilen);
-		if (connfd < 0) {
-			if (errno == EINTR)
-				continue;
-			else
-				printf("accept error:%s (errno:%d)\n", strerror(errno), errno);
+	str_echo(listenid,&cliaddr,sizeof(cliaddr));
 
-		}
-		if ((childpid = fork()) == 0) {
-			close(listenid);
-			str_echo(connfd);
-			exit(0);
-		}			
-		close(connfd);
-	}
 }
 
 
-void str_echo(int sockfd) {
-	ssize_t n;
+void str_echo(int sockfd,struct sockaddr_in * cliaddr,socklen_t clilen) {
+	ssize_t len;
+	int n;
 	char buf[MAXLINE];
-	struct sockaddr_in clientaddr;
-	socklen_t clientLen = sizeof(clientaddr);
-	getpeername(sockfd, (struct sockaddr*)&clientaddr, &clientLen);
-	printf("connection form:%s  port:%d\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
-again:
-	while ((n = read(sockfd, buf, MAXLINE)) > 0) {
-		buf[n] = '\0';
-		write(sockfd, buf, n);
-
+	while (1) {
+		len=clilen;
+		n=recvfrom(sockfd,buf,MAXLINE,0,(struct sockaddr *)cliaddr,&len);
+		printf("connection from:%s  port:%d\n",inet_ntoa(cliaddr->sin_addr),ntohs(cliaddr->sin_port));
+		sendto(sockfd,buf,n,0,cliaddr,len);
 	}
-	if (n < 0 && errno == EINTR)
-		goto again;
-	else if (n < 0)
-		printf("str_echo:read error");
 }
 
-void sig_chid(int signo) {
-	pid_t pid;
-	int stat;
-	while ((pid = wait(&stat)) > 0)
-		printf("child:%d terminated", pid);
-	return;
-}
+// void sig_chid(int signo) {
+// 	pid_t pid;
+// 	int stat;
+// 	while ((pid = wait(&stat)) > 0)
+// 		printf("child:%d terminated", pid);
+// 	return;
+// }
